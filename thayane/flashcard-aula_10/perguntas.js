c4fdef76aba5d96733379cef75c12ee409daee5d
htthayane <161845904+htthayane@users.noreply.github.com>
2024-11-01 10:55:38 -0300

    Add files via upload

---
+++ thayane/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sistema solar',
+    'Como são as estrelas?',
+    'Corpos celestes com formato esférico,brilhantes e muito quentes, compostas por plasma e formadas por gases e poeira.'
+)
+
+criaCartao(
+    'Sistema solar',
+    'Como e o sol?',
+    'É uma estrela anã que os demais corpos celestes do Sistema orbitam. Ele é formado por gses e não dipõe de nenhuma superfíciw sólida.'
+)
+
+criaCartao(
+    'Sitema solar',
+    'O que é o sistema solar?',
+    'Ele formado por oito planetas, asteroides, cometas, meteoroides e outros corpos celestes que orbitam o Sol. A estrela central do sistema Solar é o Sol.'
+)
+
+criaCartao(
+    'Sistema Solar',
+    'O que tem dentro a Lua?',
+    'A Lua possui um núcleo interno sólido e rico em ferro com 240 km de raio e um núcleo externo fluindo compostocessencialmente por ferro em fusão e com raio de aproximadamente 300 km. '
+)
